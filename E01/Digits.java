31f82c6e15cee68d1d38db94751dd0a366225cc3
pablo3620 <pablopaulsen3620@gmail.com>
2022-01-12 10:35:06 +1300

    repeated input

--- E01/Digits.java
+++ E01/Digits.java
@@ -4,49 +4,53 @@ public class Digits {
     public static void main(String[] args) {
         System.out.println("enter question, base, and number to use");
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
+
+        while (scan.hasNextLine()) {
+            String input = scan.nextLine();
+
+            char a = ' ';
+            int b = 0, c = 0;
+            Scanner tokens = new Scanner(input);
+            try {
+                a = tokens.next().toLowerCase().charAt(0);
+                b = tokens.nextInt();
+                c = tokens.nextInt();
+                tokens.close();
+                if (a != 'a' && a != 'b')
+                    throw new Exception();
+            } catch (Exception e) {
+                System.out.println("Bad line: " + input);
+            }
+            switch (a) {
+                case 'a':
+                    int repeatedCount = 0;
+                    int maxRepeat = 0;
+                    int maxRepLocation = 0;
+                    for (int i = c - 1; i > b; i--) {
+                        if (checkRepeated(i, b))
+                            repeatedCount++;
+                        else {
+                            repeatedCount = 0;
+                        }
+                        if (repeatedCount >= maxRepeat) {
+                            maxRepeat = repeatedCount;
+                            maxRepLocation = i;
+                        }
+                    }
+                    System.out.println(maxRepLocation + " " + maxRepeat);
+                    break;
+                case 'b':
+                    boolean exit = true;
+                    for (int i = 1; exit; i++) {
+                        if (checkRepeated(i, b) && checkRepeated(i, c)) {
+                            System.out.println(i);
+                            exit = false;
+                        }
+                    }
+                    break;
+            }
+        }
         scan.close();
-        char a = ' ';
-        int b = 0, c = 0;
-        Scanner tokens = new Scanner(input);
-        try {
-            a = tokens.next().toLowerCase().charAt(0);
-            b = tokens.nextInt();
-            c = tokens.nextInt();
-            tokens.close();
-            if (a != 'a' && a != 'b')
-                throw new Exception();
-        } catch (Exception e) {
-            System.out.println("Bad line: " + input);
-        }
-        switch (a) {
-            case 'a':
-                int repeatedCount = 0;
-                int maxRepeat = 0;
-                int maxRepLocation = 0;
-                for (int i = c - 1; i > b; i--) {
-                    if (checkRepeated(i, b))
-                        repeatedCount++;
-                    else {
-                        repeatedCount = 0;
-                    }
-                    if (repeatedCount > maxRepeat) {
-                        maxRepeat = repeatedCount;
-                        maxRepLocation = i;
-                    }
-                }
-                System.out.println(maxRepLocation + " " + maxRepeat);
-                break;
-            case 'b':
-                boolean exit = true;
-                for (int i = 1; exit; i++) {
-                    if (checkRepeated(i, b) && checkRepeated(i, c)) {
-                        System.out.println(i);
-                        exit = false;
-                    }
-                }
-                break;
-        }
 
     }
 
