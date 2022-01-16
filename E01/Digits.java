f62d49a63f024959ef3d9d65707b5289026273c8
pablo3620 <pablopaulsen3620@gmail.com>
2022-01-17 10:03:55 +1300

    git ignore

--- E01/Digits.java
+++ E01/Digits.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Digits {
     public static void main(String[] args) {
-        System.out.println("enter question, base, and number to use");
+        // System.out.println("enter question, base, and number to use");
         Scanner scan = new Scanner(System.in);
 
         while (scan.hasNextLine()) {
@@ -15,41 +15,45 @@ public class Digits {
                 a = tokens.next();
                 b = tokens.nextInt();
                 c = tokens.nextInt();
+                if (tokens.hasNext()) {
+                    tokens.close();
+                    throw new Exception();
+                }
                 tokens.close();
                 if (!a.equals("A") && !a.equals("B"))
                     throw new Exception();
+                switch (a) {
+                    case "A":
+                        int repeatedCount = 0;
+                        int maxRepeat = 0;
+                        int maxRepLocation = 0;
+                        for (int i = c - 1; i > b; i--) {
+                            if (checkRepeated(i, b))
+                                repeatedCount++;
+                            else {
+                                repeatedCount = 0;
+                            }
+                            if (repeatedCount >= maxRepeat) {
+                                maxRepeat = repeatedCount;
+                                maxRepLocation = i;
+                            }
+                        }
+                        System.out.println(maxRepLocation + " " + maxRepeat);
+                        break;
+                    case "B":
+                        boolean exit = true;
+                        for (int i = 1; exit; i++) {
+                            if (checkRepeated(i, b) && checkRepeated(i, c)) {
+                                System.out.println(i);
+                                exit = false;
+                            }
+                        }
+                        break;
+                }
             } catch (Exception e) {
                 System.out.println("Bad line: " + input);
+            }
 
-            }
-            switch (a) {
-                case "A":
-                    int repeatedCount = 0;
-                    int maxRepeat = 0;
-                    int maxRepLocation = 0;
-                    for (int i = c - 1; i > b; i--) {
-                        if (checkRepeated(i, b))
-                            repeatedCount++;
-                        else {
-                            repeatedCount = 0;
-                        }
-                        if (repeatedCount >= maxRepeat) {
-                            maxRepeat = repeatedCount;
-                            maxRepLocation = i;
-                        }
-                    }
-                    System.out.println(maxRepLocation + " " + maxRepeat);
-                    break;
-                case "B":
-                    boolean exit = true;
-                    for (int i = 1; exit; i++) {
-                        if (checkRepeated(i, b) && checkRepeated(i, c)) {
-                            System.out.println(i);
-                            exit = false;
-                        }
-                    }
-                    break;
-            }
         }
         scan.close();
 
